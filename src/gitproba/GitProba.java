2c86c1ea126ca49361245281ea61963099028fbb
CsoltiP <csolti@szamalk-szalezi.hu>
2024-05-04 15:04:07 +0200

    kiiras()

--- src/gitproba/GitProba.java
+++ src/gitproba/GitProba.java
@@ -3,6 +3,10 @@ package gitproba;
 public class GitProba {
 
     public static void main(String[] args) {
+        kiiras();
+    }
+
+    private static void kiiras() {
         System.out.println("Helló Git!");
     }
     
